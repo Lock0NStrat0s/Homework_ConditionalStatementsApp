0d815d52b4cb8287bdb1fa45a34e0510edef5e30
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-10 00:53:57 -0600

    code complete.

--- Homework_ConditionalStatements/Program.cs
+++ Homework_ConditionalStatements/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//ask for their name
+Console.Write("What is your name: ");
+string? firstName = Console.ReadLine();
+
+//using if statement determine if user is the professor or student and welcome them
+
+//if (firstName.ToLower() == "tim" || firstName.ToLower() == "timothy")
+//{
+//    Console.WriteLine("Welcome Professor!");
+//}
+//else
+//{
+//    Console.WriteLine("Welcome Student!");
+//}
+
+//now do the same with switch statement
+
+switch (firstName.ToLower())
+{
+    case "tim" or "timothy":
+        Console.WriteLine("Welcome Professor!");
+        break;
+    default:
+        Console.WriteLine("Welcome Student!");
+        break;
+}
